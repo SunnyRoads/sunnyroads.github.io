d7f5fbbefc952c492e7beea9e9b06171416a00a7
SunnyRoads <sarah.anderssonskan@gmail.com>
2026-02-03 20:25:05 -0600

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,5 +1,6 @@
-
 const button = document.getElementById("lavaButton");
+
+let redirected = false;
 
 // Show button after exactly 6 seconds
 setTimeout(() => {
@@ -7,12 +8,25 @@ setTimeout(() => {
   button.style.pointerEvents = "auto";
 }, 6000);
 
-// On click → fade out → go to main site
+// Auto redirect after 10 seconds (if no click)
+setTimeout(() => {
+  if (!redirected) {
+    fadeAndGo();
+  }
+}, 10000);
+
+// Click handler
 button.addEventListener("click", () => {
+  redirected = true;
+  fadeAndGo();
+});
+
+// Fade out + redirect
+function fadeAndGo() {
   document.body.style.transition = "opacity 1s ease";
   document.body.style.opacity = "0";
 
   setTimeout(() => {
     window.location.href = "main.html";
   }, 1000);
-});
+}
