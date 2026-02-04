89935e1073b53dae8fca7ba2eb8858b5096332e5
SunnyRoads <sarah.anderssonskan@gmail.com>
2026-02-04 07:59:19 -0600

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,10 +1,14 @@
-// Show coffee at 7 seconds
+const coffeeVideo = document.getElementById("coffeeVideo");
+const coffeeLayer = document.querySelector(".coffee");
+
+// Start coffee at exactly 7 seconds
 setTimeout(() => {
-  const coffeeLayer = document.querySelector('.coffee');
-  coffeeLayer.classList.add('show');
+  coffeeLayer.classList.add("show");
+  coffeeVideo.currentTime = 0;
+  coffeeVideo.play();
 }, 7000);
 
 // Auto redirect at 12 seconds
 setTimeout(() => {
-  window.location.href = 'main.html';
+  window.location.href = "main.html";
 }, 12000);
