e0c845ddfe979b8b55ae4f33d850f61982649a46
SunnyRoads <sarah.anderssonskan@gmail.com>
2026-02-05 15:56:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-const video = document.getElementById("intro-video");
+const video1 = document.getElementById("video-1");
+const video2 = document.getElementById("video-2");
 
+// Start first video after 3 seconds
 setTimeout(() => {
-  video.play().catch(() => {});
-  video.style.opacity = "1";
+  video1.play().catch(() => {});
+  video1.style.opacity = "1";
 }, 3000);
+
+// Crossfade to second video near the end of video 1
+video1.addEventListener("timeupdate", () => {
+  if (
+    video1.duration &&
+    video1.currentTime > video1.duration - 1.2
+  ) {
+    if (video2.paused) {
+      video2.play().catch(() => {});
+      video2.style.opacity = "1";
+      video1.style.opacity = "0";
+    }
+  }
+});
